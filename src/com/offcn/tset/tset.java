d5a608b8751584b4a40af8871c2de34225374104
root <1187464984@qq.com>
2019-06-28 14:24:01 +0800

    centos第二次呀屎拉你

--- src/com/offcn/tset/tset.java
+++ src/com/offcn/tset/tset.java
@@ -3,6 +3,7 @@ package com.offcn.tset;
 public class tset {
 
     public static void main(String[] args) {
+        System.out.println("快叫非凡哥");
         System.out.println("听不见我就再说！！！");
         System.out.println("知道你听见为止！！！");
         System.out.println("呀屎拉你！梁非凡！！！！！");
